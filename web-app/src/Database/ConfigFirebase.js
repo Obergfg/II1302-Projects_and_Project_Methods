e6dfab4a60cc91854f4553f6dd05def3e6bc8d72
Fredrik Öberg <fredrik.oberg@gmx.com>
2020-05-12 14:10:43 +0200

    the farm test

---
+++ web-app/src/Database/ConfigFirebase.js
@@ -0,0 +1,13 @@
+//Comment
+
+const CONFIG = {
+    apiKey: "AIzaSyAEa5XaupOAhA5xfKMq4XZ4ECk2iMwJrQY",
+    authDomain: "thefarmdb.firebaseapp.com",
+    databaseURL: "https://thefarmdb.firebaseio.com/",
+    projectId: "thefarmdb",
+    storageBucket: "thefarmdb.appspot.com",
+    messagingSenderId: "391118619238",
+    appId: "1:391118619238:web:fe765a9792e0296d7da3a7"
+};
+
+export default CONFIG;
